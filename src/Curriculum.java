3174c5360bfd7894ad145ccc63d54cd62d25153d
Ezra Micah Malsi <66657798+ZaZra03@users.noreply.github.com>
2022-11-30 16:14:27 +0800

    Update Curriculum.java

--- src/Curriculum.java
+++ src/Curriculum.java
@@ -1,4 +1,28 @@
+import java.util.List;
 
 public class Curriculum {
-
+	//Class fields
+	private int idCounter;
+	private int id;
+	private String yearEffectivity;
+	private List<Course> listCourse;
+	private Program program;
+	
+	//Parameterized Constructor
+	public Curriculum(String yearEffectivity) {
+		this.yearEffectivity = yearEffectivity;
+	}
+	
+	//Methods
+	public void AddCourse(Course course, int term, int semester) {
+		
+	}
+	
+	public void RemoveCourse(Course course) {
+		
+	}
+	
+	public String GetFullDetails() {
+		return "";
+	}
 }
