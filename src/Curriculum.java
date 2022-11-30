f99f3d0efc99823bbb3c89b3ece9319bad1a54f5
Mark Christian Banagan <112263674+ChrisOfReality@users.noreply.github.com>
2022-11-30 16:57:21 +0800

    AddCourse and CheckUnitCount

--- src/Curriculum.java
+++ src/Curriculum.java
@@ -1,11 +1,11 @@
-import java.util.List;
+import java.util.*;
 
 public class Curriculum {
 	//Class fields
 	private int idCounter;
 	private int id;
 	private String yearEffectivity;
-	private List<Course> listCourse;
+	private List<Course> listCourses = new ArrayList<Course>();
 	private Program program;
 	
 	//Parameterized Constructor
@@ -16,12 +16,31 @@ public class Curriculum {
 	}
 	
 	//Methods
-	public void AddCourse(Course course, int term, int semester) {
-		
+	public void AddCourse(Course course, int year, int semester) throws Exception {
+		Course tempCourse = course;
+		tempCourse.setYear(year);
+		tempCourse.setSemester(semester);
+		if (!this.CheckUnitCount(tempCourse)) {
+			this.listCourses.add(tempCourse);
+		} else {
+			throw new Exception("\nNumber of Units exceeds the limit unit for the semester");
+		}
 	}
 	
 	public void RemoveCourse(Course course) {
 		
+	}
+	
+	private boolean CheckUnitCount(Course course) {
+		int count = course.getUnits();
+		for (Course currentCourse : this.listCourses) {
+			if (currentCourse.getYear() == course.getYear()) {
+				if (currentCourse.getSemester() == course.getSemester()) {
+					count += currentCourse.getUnits();
+				}
+			}
+		}
+		return count >= 26;
 	}
 	
 	public String GetFullDetails() {
