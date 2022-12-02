76a08497b3379f8c34add8f757da13698fe8ac1c
Mark Christian Banagan <112263674+ChrisOfReality@users.noreply.github.com>
2022-12-02 16:33:54 +0800

    Update Course.java

--- src/Course.java
+++ src/Course.java
@@ -1,63 +1,38 @@
-import java.util.*;
 
 public class Course {
 	// Class Fields
+	private static int idCounter = 1;
+	private int id;
 	private String courseCode;
 	private String description;
 	private int units;
 	private String preRequisite;
 	private String coRequisite;
-	private List<Curriculum> listCurriculums = new ArrayList<Curriculum>();
-	private int year;
-	private int semester;
 	
 	// Getters
 	public int getUnits() {
 		return this.units;
 	}
 
-	public int getYear() {
-		return this.year;
-	}
-
-	public int getSemester() {
-		return this.semester;
-	}
-
-	// Setters
-	public void setYear(int year) {
-		this.year = year;
-	}
-
-	public void setSemester(int semester) {
-		this.semester = semester;
-	}
-
 	// Parameterized Constructor
 	public Course(String courseCode, String description, int units, String preRequisite, String coRequisite) {
+		this.id = idCounter;
 		this.courseCode = courseCode;
 		this.description = description;
 		this.units = units;
 		this.preRequisite = preRequisite;
 		this.coRequisite = coRequisite;
+		idCounter++;
 	}
 
 	// Methods
-	public void AddCurriculum(Curriculum curriculum) {
-		if (!this.listCurriculums.contains(curriculum)) {
-			this.listCurriculums.add(curriculum);
-		}
-	}
-
-	public void RemoveCurriculum(Curriculum curriculum) {
-		if (this.listCurriculums.contains(curriculum)) {
-			this.listCurriculums.remove(curriculum);
-		}
-	}
-
 	public String GetCourseDetails() {
-		return "COURSE CODE : " + this.courseCode + "\nDESCRIPTION : " + this.description + "\nUNITS : " + this.units
+		return "\nCOURSE ID : " + this.id + "\nCOURSE CODE : " + this.courseCode + "\nDESCRIPTION : " + this.description;
+	}
+	
+	public String GetFullDetails() {
+		return "\nCOURSE ID : " + this.id + "\nCOURSE CODE : " + this.courseCode + "\nDESCRIPTION : " + this.description + "\nUNITS : " + this.units
 				+ "\nPRE-REQUISITE : " + (this.preRequisite.isBlank() ? "NONE" : this.preRequisite) + "\nCO-REQUISITE : "
-				+ (this.coRequisite.isBlank() ? "NONE" : this.coRequisite) + "\n";
+				+ (this.coRequisite.isBlank() ? "NONE" : this.coRequisite);
 	}
 }
